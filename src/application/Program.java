050012444343d0990c8ed99862ea320a568d69ae
maurochabrito <mauriciorochasic@gmail.com>
2022-12-08 02:15:26 -0300

    Finished thermodynamics model

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import thermodynamicsModel.GammaModel;
+import thermodynamicsModel.VapourPressureModel;
+import thermodynamicsModel.impl.Antoine;
+import thermodynamicsModel.impl.MargulesGammaModel;
+
 public class Program {
 
 	public static void main(String[] args) {
-		VaporPressureModel vpm1 = new Antoine()
+		VapourPressureModel vpm1 = new Antoine(18.9119,3803.98,-41.68);
+		VapourPressureModel vpm2 = new Antoine(18.3036,3816.44,-46.13);
+		GammaModel gm = new MargulesGammaModel(1.6022,0.7947);
+		Double externalPressure = 760.0; //mmHg
 	}
 
 }
