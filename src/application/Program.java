8e37ad0d5be47ab5ff599b559912f7ae31833d9c
maurochabrito <mauriciorochasic@gmail.com>
2022-12-08 02:21:21 -0300

    First test

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import thermodynamicsModel.GammaModel;
+import thermodynamicsModel.RaoultLaw;
 import thermodynamicsModel.VapourPressureModel;
 import thermodynamicsModel.impl.Antoine;
 import thermodynamicsModel.impl.MargulesGammaModel;
@@ -12,6 +13,9 @@ public class Program {
 		VapourPressureModel vpm2 = new Antoine(18.3036,3816.44,-46.13);
 		GammaModel gm = new MargulesGammaModel(1.6022,0.7947);
 		Double externalPressure = 760.0; //mmHg
+		Double X = 0.2;
+		Double Y = RaoultLaw.iterativeY(X, vpm1, vpm2, gm, externalPressure);
+		System.out.println("X = "+ X +"\nY = "+ Y);
 	}
 
 }
